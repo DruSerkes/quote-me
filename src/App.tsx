57f89635465b635071a7ff1d8fc44903adbf818b
Dru Serkes <DruSerkes@gmail.com>
2020-09-25 16:07:14 -0400

    add styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 import Quote from './Quote';
+import { H1 } from './styledComponents';
 
 const App: React.FC = () => {
 	return (
 		<div className="App">
+			<H1>Quote Generator</H1>
 			<Quote />
 		</div>
 	);
--- src/styledComponents.ts
+++ src/styledComponents.ts
@@ -2,8 +2,7 @@ import styled from 'styled-components';
 
 export const Main = styled.main`
 	padding: 3rem;
-	width: 75%;
-	margin: 0 auto;
+	width: 100%;
 	border-radius: 6px;
 	opacity: 0.7;
 	background: whitesmoke;
@@ -18,12 +17,34 @@ export const BlockQuote = styled.blockquote`
 
 export const Button = styled.button`
 	display: block;
-	margin: 1rem 0 0 75%;
-	padding: 0.5rem 1rem;
-	background-color: rgb(66, 184, 221);
+	cursor: pointer;
+	margin: 1rem 0 0 auto;
+	padding: 0.2rem 0.7rem;
+	background-color: black;
+	border-color: white;
 	color: white;
 	font-size: 1.2rem;
 	font-family: copperplate;
-	border-radius: 3px;
+	border-radius: 4px;
 	text-shadow: 0 1px 1px rgba(0, 0, 0, 0.2);
+	box-shadow: 1px 1px rgba(0, 0, 0, 0.2);
+	&:hover {
+		border-left: none;
+		border-right: none;
+		border-top-width: 2px;
+		border-bottom-width: 2px;
+		border-top-style: solid;
+		border-bottom-style: solid;
+		border-top-color: white;
+		border-bottom-color: white;
+		background-color: black;
+		transition: all 0.5s;
+		color: white;
+		transform: scale(1.1, 1);
+	}
+`;
+
+export const H1 = styled.h1`
+	padding: 0.8rem auto 0;
+	color: white;
 `;
