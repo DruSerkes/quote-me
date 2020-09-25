24d21a3ef22e793d941a73cc0161a899c365f82f
Dru Serkes <DruSerkes@gmail.com>
2020-09-25 14:22:24 -0400

    add styled components

--- src/Quote.tsx
+++ src/Quote.tsx
@@ -1,6 +1,35 @@
 import React, { useState, useEffect } from 'react';
 import Loader from 'react-loader-spinner';
 import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
+import styled from 'styled-components';
+
+const Main = styled.main`
+	padding: 3rem;
+	width: 75%;
+	margin: 0 auto;
+	border-radius: 6px;
+	opacity: 0.7;
+	background: whitesmoke;
+`;
+
+const BlockQuote = styled.blockquote`
+	text-align: center;
+	font-size: 1.3rem;
+	color: darkslategray;
+	display: block;
+`;
+
+const Button = styled.button`
+	display: block;
+	margin: 1rem 0 0 75%;
+	padding: 0.5rem 1rem;
+	background-color: rgb(66, 184, 221);
+	color: white;
+	font-size: 1.2rem;
+	font-family: copperplate;
+	border-radius: 3px;
+	text-shadow: 0 1px 1px rgba(0, 0, 0, 0.2);
+`;
 
 interface QuoteData {
 	content: string;
@@ -36,16 +65,24 @@ const Quote: React.FC = () => {
 		setLoading(true);
 	};
 
-	if (loading) {
-		return <Loader type="ThreeDots" color="#00BFFF" height={80} width={80} />
-	}
+	// if (loading) {
+	// 	return <Loader type="ThreeDots" color="#00BFFF" height={80} width={80} />
+	// }
 
 	return (
-		<main className="Quote">
-			<blockquote>"{quote?.content}"</blockquote>
-			<cite>{quote?.author}</cite>
-			<button onClick={getNewQuote}>Gimme Quote</button>
-		</main>
+		<div className="Quote">
+			<Main className="Quote-Main">
+				{loading 
+				? <Loader type="ThreeDots" color="#00BFFF" height={80} width={80} />
+				:	<>
+						<BlockQuote>"{quote?.content}"</BlockQuote>
+						<cite>{quote?.author}</cite> 
+					</>
+				}
+			</Main>
+
+			<Button onClick={getNewQuote}>Gimme Quote</Button>
+		</div>
 	);
 };
 
