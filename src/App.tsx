ebb05137ad795b8df9b13d3f07cb64f84cb23f2f
Dru Serkes <DruSerkes@gmail.com>
2020-09-25 19:03:36 -0400

    add footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Quote from './Quote';
+import Footer from './Footer';
 import { H1 } from './styledComponents';
 
 const App: React.FC = () => {
@@ -8,6 +9,7 @@ const App: React.FC = () => {
 		<div className="App">
 			<H1>Quote Me</H1>
 			<Quote />
+			<Footer />
 		</div>
 	);
 };
---
+++ src/Footer.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Footer = () => {
+	return (
+		<footer className="Footer">
+			<p>
+				{' '}
+				Built by{' '}
+				<a href="https://github.com/druserkes" target="_blank" rel="noopener noreferrer">
+					Dru Serkes
+				</a>
+				&copy; 2020
+			</p>
+		</footer>
+	);
+};
+
+export default Footer;
