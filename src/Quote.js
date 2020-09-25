163b478084c4445d502772e3ed8dd863c2811e47
Dru Serkes <DruSerkes@gmail.com>
2020-09-25 02:24:48 -0400

    create Quote component

--- src/Quote.js
+++ src/Quote.js
@@ -1,9 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
+export default (Quote = () => {
+	const [ quote, setQuote ] = useState(null);
+	const [ loading, setLoading ] = useState(true);
 
-export default Quote = () => {
-    return (
-        <h1>I AM THE QUOTE</h1>
-    )
-}
+	useEffect(
+		() => {
+			const getQuote = async () => {
+				setLoading(true);
+				const params = new URLSearchParams({
+					lang   : 'en',
+					format : 'json',
+					method : 'getQuote'
+				});
+				const res = await window.fetch('https://forismatic.com/en/api/' + params.toString());
+				console.log('res == ', res);
+				const data = res.json();
+				console.log('data ==', data);
+				setQuote(data);
+				setLoading(false);
+			};
+			if (!quote) getQuote();
+		},
+		[ quote ]
+	);
 
+	if (loading) return <h1>Loading - add a spinner here</h1>;
+
+	return (
+		<main className="Quote">
+			<h1>{quote}</h1>
+			<button onClick={() => setQuote(null)}>Gimme Quote</button>
+		</main>
+	);
+});
