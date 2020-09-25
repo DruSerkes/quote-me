ea782dad729391b4395d53e39a385926342911d3
Dru Serkes <DruSerkes@gmail.com>
2020-09-25 14:35:58 -0400

    add quote style, remove comments

--- src/Quote.tsx
+++ src/Quote.tsx
@@ -2,35 +2,6 @@ import React, { useState, useEffect } from 'react';
 import Loader from 'react-loader-spinner';
 import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
 import {Button, BlockQuote, Main} from './styledComponents'
-// import styled from 'styled-components';
-
-// const Main = styled.main`
-// 	padding: 3rem;
-// 	width: 75%;
-// 	margin: 0 auto;
-// 	border-radius: 6px;
-// 	opacity: 0.7;
-// 	background: whitesmoke;
-// `;
-
-// const BlockQuote = styled.blockquote`
-// 	text-align: center;
-// 	font-size: 1.3rem;
-// 	color: darkslategray;
-// 	display: block;
-// `;
-
-// const Button = styled.button`
-// 	display: block;
-// 	margin: 1rem 0 0 75%;
-// 	padding: 0.5rem 1rem;
-// 	background-color: rgb(66, 184, 221);
-// 	color: white;
-// 	font-size: 1.2rem;
-// 	font-family: copperplate;
-// 	border-radius: 3px;
-// 	text-shadow: 0 1px 1px rgba(0, 0, 0, 0.2);
-// `;
 
 interface QuoteData {
 	content: string;
