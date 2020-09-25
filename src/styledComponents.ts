571aec94ceeb8863bd8e9a029c0714b8774189f1
Dru Serkes <DruSerkes@gmail.com>
2020-09-25 14:28:31 -0400

    export/import styled components

---
+++ src/styledComponents.ts
@@ -0,0 +1,29 @@
+import styled from 'styled-components';
+
+export const Main = styled.main`
+	padding: 3rem;
+	width: 75%;
+	margin: 0 auto;
+	border-radius: 6px;
+	opacity: 0.7;
+	background: whitesmoke;
+`;
+
+export const BlockQuote = styled.blockquote`
+	text-align: center;
+	font-size: 1.3rem;
+	color: darkslategray;
+	display: block;
+`;
+
+export const Button = styled.button`
+	display: block;
+	margin: 1rem 0 0 75%;
+	padding: 0.5rem 1rem;
+	background-color: rgb(66, 184, 221);
+	color: white;
+	font-size: 1.2rem;
+	font-family: copperplate;
+	border-radius: 3px;
+	text-shadow: 0 1px 1px rgba(0, 0, 0, 0.2);
+`;
