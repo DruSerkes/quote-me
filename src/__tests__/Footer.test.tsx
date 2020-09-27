33faa188f9c13472414d65cd5dc5b6634e897372
Dru Serkes <DruSerkes@gmail.com>
2020-09-27 02:05:48 -0400

    add App tests

---
+++ src/__tests__/Footer.test.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import App from '../App';
+import { render } from '@testing-library/react';
+
+describe('App tests', () => {
+	it('should render without breaking', () => {
+		const { getByText } = render(<App />);
+	});
+
+	it('should match snapshot', () => {
+		const { asFragment } = render(<App />);
+		expect(asFragment()).toMatchSnapshot();
+	});
+
+	it('should render an H1', () => {
+		const { getByText } = render(<App />);
+		expect(getByText('Quote Me')).toBeInTheDocument();
+	});
+});
