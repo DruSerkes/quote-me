7e99bc72e31e14515a619faed6109628e2144335
Dru Serkes <druserkes@gmail.com>
2023-02-11 18:13:13 -0500

    General cleanup

--- src/Quote.tsx
+++ src/Quote.tsx
@@ -8,74 +8,69 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTwitter } from '@fortawesome/free-brands-svg-icons';
 
 interface QuoteData {
-	content: string;
-	author: string;
+  content: string;
+  author: string;
 }
 
 const Quote: React.FC = () => {
-	const [ quote, setQuote ] = useState<QuoteData | null>(null);
-	const [ loading, setLoading ] = useState(true);
-	const [ toolTip, setToolTip ] = useState('Copy');
-	const text = useRef('');
+  const [quote, setQuote] = useState<QuoteData | null>(null);
+  const [loading, setLoading] = useState(true);
+  const [toolTip, setToolTip] = useState('Copy');
+  const text = useRef('');
 
-	useEffect(
-		() => {
-			const getQuote = async () => {
-				try {
-					setLoading(true);
-					const res = await fetch('https://api.quotable.io/random');
-					const { content, author } = await res.json();
-					setQuote({ content, author });
-					text.current = `"${content}" -${author}`;
-				} catch (e) {
-					console.log(e);
-					setQuote(null);
-				} finally {
-					setTimeout(() => setLoading(false), 500);
-				}
-			};
-			if (!quote) getQuote();
-		},
-		[ quote ]
-	);
+  useEffect(() => {
+    const getQuote = async () => {
+      try {
+        setLoading(true);
+        const res = await fetch('https://api.quotable.io/random');
+        const { content, author } = await res.json();
+        setQuote({ content, author });
+        text.current = `"${content}" -${author}`;
+      } catch (e) {
+        console.log(e);
+        setQuote(null);
+      } finally {
+        setTimeout(() => setLoading(false), 500);
+      }
+    };
+    if (!quote) getQuote();
+  }, [quote]);
 
-	const getNewQuote = () => {
-		setQuote(null);
-		setLoading(true);
-	};
+  const getNewQuote = () => {
+    setQuote(null);
+    setLoading(true);
+  };
 
-	const handleCopy = () => {
-		copyText(text.current);
-		setToolTip('Copied!');
-		setTimeout(() => setToolTip('Copy'), 1500);
-	};
+  const handleCopy = () => {
+    copyText(text.current);
+    setToolTip('Copied!');
+    setTimeout(() => setToolTip('Copy'), 1500);
+  };
 
-	return (
-		<div className="Quote">
-			<Main className="Quote-Main">
-				{loading ? (
-					<Loader type="ThreeDots" color="#00BFFF" height={80} width={80} />
-				) : (
-					<ShowQuote quote={quote} />
-				)}
-			</Main>
-			<section className="Quote-Buttons">
-				<Button>
-					<a
-						id="tweet-quote"
-						title="tweet this code"
-						href={`https://twitter.com/intent/tweet?text=${text.current}`}
-						target="_blank"
-						rel="noopener noreferrer"
-						className="Tweet-Button">
-						<FontAwesomeIcon icon={faTwitter} />
-					</a>
-				</Button>
-				<Button onClick={handleCopy}>{toolTip}</Button>
-				<Button onClick={getNewQuote}>New Quote</Button>
-			</section>
-		</div>
-	);
+  return (
+    <div className="Quote">
+      <Main className="Quote-Main">
+        {loading
+          ? <Loader type="ThreeDots" color="#00BFFF" height={80} width={80} />
+          : <ShowQuote quote={quote} />}
+      </Main>
+      <section className="Quote-Buttons">
+        <Button>
+          <a
+            id="tweet-quote"
+            title="tweet this code"
+            href={`https://twitter.com/intent/tweet?text=${text.current}`}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="Tweet-Button">
+            <FontAwesomeIcon icon={faTwitter} />
+          </a>
+        </Button>
+        <Button onClick={handleCopy}>{toolTip}</Button>
+        <Button onClick={getNewQuote}>New Quote</Button>
+      </section>
+    </div>
+  );
 };
 
 export default Quote;
